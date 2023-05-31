36512441b1926c884aa034c72873069fc1556be9
loqumi <loqumihh@gmail.com>
2023-05-31 16:20:45 +0200

    added modal for image cell

--- src/components/ImageCell.tsx
+++ src/components/ImageCell.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
+import {Modal} from "./Modal";
 
 export const ImageCell = ({src}: {src: string}) => {
+    const [isOpen, setIsOpen] = useState(false)
+    const handleClose = () => setIsOpen(false)
+
     const handleClick = (event: React.MouseEvent) => {
         event.stopPropagation()
-        console.log('click!')
+        setIsOpen(true)
     }
-    return <img src={process.env.REACT_APP_API_IMG + src} alt="poster" height={300} onClick={handleClick} />
+    return <>
+        <img src={process.env.REACT_APP_API_IMG + src} alt="poster" height={300} onClick={handleClick} />
+
+        <Modal open={isOpen} onClose={handleClose}>
+            <img src={process.env.REACT_APP_API_IMG + src} alt="poster" height={500} />
+        </Modal>
+    </>
 }
---
+++ src/components/Modal.tsx
@@ -0,0 +1,29 @@
+import {IconButton, Modal as MUIModal, ModalProps, Paper, Stack, styled} from "@mui/material";
+import React from "react";
+import {Close} from "@mui/icons-material";
+
+const StyledPaper = styled(Paper)(({theme}) => ({
+    padding: theme.spacing(2)
+}))
+
+const StyledModal = styled(MUIModal)(({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+}))
+
+export const Modal = ({children, ...props}: {children: React.ReactNode, onClose: () => void} & Omit<ModalProps, 'children' | 'onClose'>) => {
+    const handleClick = () => props.onClose()
+
+    return <StyledModal {...props}>
+            <StyledPaper>
+                <Stack alignItems='flex-end' spacing={2}>
+                    <IconButton onClick={handleClick}>
+                        <Close />
+                    </IconButton>
+                {children}
+                </Stack>
+            </StyledPaper>
+    </StyledModal>
+}
