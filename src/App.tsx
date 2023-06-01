715b3834b94e3e46ae4c80daa3ef6642f41dff1d
loqumi <loqumihh@gmail.com>
2023-06-01 12:00:10 +0200

    added pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {DataGrid, GridSortModel} from '@mui/x-data-grid';
+import {DataGrid, GridPaginationModel, GridSortModel} from '@mui/x-data-grid';
 import {Movie, MovieDBResponse} from "./types/api";
 import {AppDecorator} from "./theme/AppDecorator";
 import {columns} from "./config/columns";
@@ -19,30 +19,40 @@ function App() {
 const Content = () => {
     const [movies, setMovies] = useState<Movie[]>([])
     const [sortModel, setSortModel] = useLocalStorage<GridSortModel | undefined>('SortModel', undefined)
+    const [paginationModel, setPaginationModel] = useLocalStorage<GridPaginationModel | undefined>('PaginationModel', {pageSize: 20, page: 0})
+    const url = process.env.REACT_APP_API_URL || ''
+    const [rowCount, setRowCount] = useState(0)
+    const [page, setPage] = useState(1)
 
     useEffect(() => {
         (async () => {
-            const url = process.env.REACT_APP_API_URL
-            if (!url) return
-            const data = await fetch(url).then(res => res.json()) as MovieDBResponse
+            const page = (paginationModel?.page ? paginationModel.page : 0) + 1;
+            const data = await fetch(url + `&page=${page}`).then(res => res.json()) as MovieDBResponse
             setMovies(data.results)
+            setRowCount(data.total_pages)
         })()
     }, [])
+
+    const handleChangePage = async (model: GridPaginationModel) => {
+        const data = await fetch(url + `&page=${Number(model?.page) + 1}`).then(res => res.json()) as MovieDBResponse
+        setMovies(data.results)
+        setPaginationModel(model)
+    }
 
   return (
     <main>
       <div style={{ height: "auto", width: '100%' }}>
           <DataGrid
-              rows={movies}
+              rows={movies || []}
               columns={columns}
               getRowHeight={(props) => 'auto'}
               onSortModelChange={setSortModel}
               sortModel={sortModel}
-              initialState={{
-                  pagination: {
-                      paginationModel: { page: 0, pageSize: 10 },
-                  },
-              }}
+              paginationMode='server'
+              rowCount={rowCount}
+              onPaginationModelChange={handleChangePage}
+              paginationModel={paginationModel}
+              pageSizeOptions={[20]}
               slots={{
                   toolbar: Toolbar
               }}
